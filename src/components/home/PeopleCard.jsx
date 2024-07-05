1d5742f79e8fa026717cabcf3cf13a8d6c4a17b8
CHINTU PRANAY <pranay.jumbarthi1905@gmail.com>
2024-07-05 20:12:44 +0530

    solved the errors

--- src/components/home/PeopleCard.jsx
+++ src/components/home/PeopleCard.jsx
@@ -5,13 +5,9 @@ export default function PeopleCard(props){
     return(
         <div className="nextpage">
         <div className="card-2">
-          <img src={PeopleCard} alt="Dinakar" className="pic-dina" />
-          {/*<img src={mask1} alt="mask-1" className="Mask-1" /> 
-          <img src={mask2} alt="mask-2" className="Mask-2" />
-          <img src={mask3} alt="mask-3" className="Mask-3" />
-          <img src={mask4} alt="mask-4" className="Mask-4" /> */}
+          <img src={props.img} alt="Dinakar" className="pic-dina" />
         </div>
-      <img src={props.img} alt="photo" />
+      
     </div>
     );
 }
