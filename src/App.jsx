94769f3f527fd2efdabec4549794f6d35928022b
CHINTU PRANAY <pranay.jumbarthi1905@gmail.com>
2024-07-05 16:43:38 +0530

    pulling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 import Home from './components/Home';
-
-
+import About from './components/about/About';
+import Projects from './components/projects/Projects';
+import Partners from './components/Partners/Partners';
+import HumanOnMars from './components/HOM/HumanOnMars';
+import News from './components/news/News';
+import Technology from './components/technology/Technology';
+import { Navbar } from './components/home/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className='main'>
-      <Home/>
-    </div>
-    
+    <Router>
+      <div className='main'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/partners' element={<Partners />} />
+          <Route path='/human-on-mars' element={<HumanOnMars />} />
+          <Route path='/news' element={<News />} />
+          <Route path='/technology' element={<Technology />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App
+export default App;
--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -1,19 +1,21 @@
+// Navbar.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
+
 export const Navbar = () => {
   return (
     <div className='nav'>
-        <h2>Mars Production</h2>
-        <ul>
-            <li id='home'>Home</li>
-            <li>About us</li>
-            <li>Project</li>
-            <li>Partner</li>
-            <li>Human on Mars</li>
-            <li>News</li>
-            <li>Technology</li>
-        </ul>
-        
+      <h2>Mars Production</h2>
+      <ul>
+        <li><Link to='/' id='home'>Home</Link></li>
+        <li><Link to='/about'>About us</Link></li>
+        <li><Link to='/projects'>Projects</Link></li>
+        <li><Link to='/partners'>Partners</Link></li>
+        <li><Link to='/human-on-mars'>Human on Mars</Link></li>
+        <li><Link to='/news'>News</Link></li>
+        <li><Link to='/technology'>Technology</Link></li>
+      </ul>
     </div>
-  )
+  );
 }
