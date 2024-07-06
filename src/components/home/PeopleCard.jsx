a8f1dafa14d4af7e9804fdb43bb11104a6403e86
Durga-prasad-1 <durgagundeti@gmail.com>
2024-07-07 01:12:56 +0530

    small change in peopleCard

--- src/components/home/PeopleCard.jsx
+++ src/components/home/PeopleCard.jsx
@@ -3,13 +3,15 @@ import './peopleCard.css';
 
 export default function PeopleCard({ name, sub, text, img }) {
     return (
-        <div className="nextpage">
+        
             <div className="card-2">
+                <div>
+                    <h2>{name}</h2>
+                    <h3>{sub}</h3>
+                    <p>{text}</p>
+                </div>
                 <img src={img} alt={name} className="pic-dina" />
-                <h2>{name}</h2>
-                <h3>{sub}</h3>
-                <p>{text}</p>
             </div>
-        </div>
+        
     );
 }
